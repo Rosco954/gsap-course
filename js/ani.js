22bbf99b3c6c79f455eaecd0ba0e4e5be363dea9
Matt Carapazza <matthew.carapazza@gmail.com>
2021-03-09 09:06:52 -0500

    added animations to header with gsap

--- js/ani.js
+++ js/ani.js
@@ -1,11 +1,116 @@
-const tween = gsap.fromTo('.heading-ani', {
-    x:0, 
-    opacity:0},
-    {
-    duration: 1,
-    x: '50%',
+let tl = gsap.timeline()
+tl.fromTo('.logo', {
+    x: -200,
+    opacity: 0
+},
+{
+    x: 0,
     opacity: 1,
-    ease: 'bounce',
-    stagger: 0.5    
-    }
-    );
+    delay: 0.2,
+    duration: 1
+})
+.fromTo('.menu', {
+    x: 200,
+    opacity: 0,
+},
+{
+    x: 0,
+    opacity: 1,
+    delay: 0.2,
+    duration: 1
+},
+'>-1.15'
+)
+.fromTo('.girl', {
+    height: 0
+},
+{
+    height: 600,
+    duration: 1.2,
+    ease: 'power3'
+},
+'>-.5'
+)
+.fromTo('.boy', {
+    height: 0
+},
+{
+    height: 600,
+    duration: 1.2,
+    ease: 'power3'
+},
+'>-.5'
+)
+.fromTo('.shape1', {
+    opacity:0,
+    scale: 0.2
+},
+{
+    opacity: 1,
+    scale: 1,
+    duration: 1.2,
+    ease: 'power2'
+},
+'>-.7'
+)
+.fromTo('.shape2', {
+    opacity:0,
+    scale: 0.2
+},
+{
+    opacity: 0.5,
+    scale: 1,
+    duration: 1.2,
+    ease: 'power2'
+},
+'>-.7'
+)
+.fromTo('.shape3', {
+    opacity:0,
+    scale: 0.2
+},
+{
+    opacity: 0.5,
+    scale: 1,
+    duration: 1.2,
+    ease: 'power2'
+},
+'>-1.15'
+)
+.fromTo('.left-ani', {
+    opacity:0,
+    x: -500,
+},
+{
+    x: 0,
+    opacity: 1,
+    duration: 1.2,
+    ease: 'power2',
+    stagger: 0.8
+},
+'>-1.15'
+)
+.fromTo('.shape2', {
+    y: -300
+},
+{
+    y: -260,
+    duration: 2,
+    ease: 'sine.inOut',
+    yoyo: true,
+    repeat: -1
+},
+'>-1.15'
+)
+.fromTo('.shape3', {
+    y: 300
+},
+{
+    y: 260,
+    duration: 2,
+    ease: 'sine.inOut',
+    yoyo: true,
+    repeat: -1
+},
+'>-2'
+)
